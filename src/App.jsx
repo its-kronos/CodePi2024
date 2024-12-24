0474fef4c4a9b0bc6fe5ffddcdaf0d825586b58e
its-kronos <anseld41@gmail.com>
2024-12-23 21:01:00 -0500

    Added fade in

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css'
+import Page1 from './Components/Page1/Page1.jsx'
+import './Components/Page1/Page1.css'
 
 function App() {
-
   return (
-    <center>
-      <h1>Lorem ipsum dolor set amet</h1>
-      <p className='Explaining_Text'>Lorem ipsum odor amet, 
-        consectetuer adipiscing elit. Tristique nisl ultricies 
-        nulla suscipit mauris sed curabitur blandit. Purus 
-        elementum natoque dictum nullam sed integer. Viverra 
-        arcu felis, morbi lacinia habitant maecenas. 
-        Ut venenatis sociosqu elementum phasellus sociosqu 
-        quisque maximus convallis.</p>
-      <button className='Next_Button'>Next</button>
-    </center>
+    <Page1></Page1>
   )
 }
 
---
+++ src/Components/Page1/Page1.jsx
@@ -0,0 +1,15 @@
+function Page1(){
+    return (<center className="PreFadeIn">
+        <h1>Lorem ipsum dolor set amet</h1>
+        <p className='Explaining_Text'>Lorem ipsum odor amet, 
+          consectetuer adipiscing elit. Tristique nisl ultricies 
+          nulla suscipit mauris sed curabitur blandit. Purus 
+          elementum natoque dictum nullam sed integer. Viverra 
+          arcu felis, morbi lacinia habitant maecenas. 
+          Ut venenatis sociosqu elementum phasellus sociosqu 
+          quisque maximus convallis.</p>
+        <button className='Next_Button'>Next</button>
+      </center>)
+}
+
+export default Page1
